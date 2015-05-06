ae77fbfb74434fb120882812675561d8ccb98ab6
Zachary Huff <zach.huff.386@gmail.com>
2015-05-06 19:30:45 -0400

    Add constants

---
+++ constants/constants.go
@@ -0,0 +1,44 @@
+// Global constants.
+package constants
+
+import (
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"time"
+)
+
+const (
+	RetryDelay       = 3 * time.Second
+	ErrLogDelay      = 3 * time.Minute
+	KeepAliveTimeout = 2 * time.Minute
+)
+
+var (
+	Version string
+)
+
+func init() {
+	ver := "unknown"
+
+	goPath := os.Getenv("GOPATH")
+	if goPath == "" {
+		return
+	}
+
+	pkgPath := path.Join(goPath, "src/github.com/pritunl/pritunl-auth")
+
+	output, err := exec.Command("git", "-C", pkgPath,
+		"rev-parse", "HEAD").Output()
+	if err != nil {
+		return
+	}
+
+	ver = strings.TrimSpace(string(output))
+	if len(ver) > 8 {
+		ver = ver[:8]
+	}
+
+	Version = ver
+}
