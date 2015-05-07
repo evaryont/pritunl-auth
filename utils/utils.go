ea626db35212f7aab4396da9c9b0806af86524fd
Zachary Huff <zach.huff.386@gmail.com>
2015-05-06 21:46:04 -0400

    Add rand str to utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,2 +1,20 @@
 // Miscellaneous utilities.
 package utils
+
+import (
+	"math/rand"
+)
+
+var (
+	chars = []rune(
+	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func RandStr(n int) (str string) {
+	strList := make([]rune, n)
+	for i := range strList {
+		strList[i] = chars[rand.Intn(len(chars))]
+	}
+	str = string(strList)
+	return
+}
