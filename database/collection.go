4a6af740f92e73294188af30a1a7886ed7f2c533
Zachary Huff <zach.huff.386@gmail.com>
2015-05-06 22:02:27 -0400

    Add find to db

--- database/collection.go
+++ database/collection.go
@@ -10,6 +10,28 @@ type Collection struct {
 	Database *Database
 }
 
+func (c *Collection) FindOne(query interface{}, result interface{}) (
+err error) {
+	err = c.Find(query).One(result)
+	if err != nil {
+		err = ParseError(err)
+		return
+	}
+
+	return
+}
+
+func (c *Collection) FindOneId(id interface{}, result interface{}) (
+err error) {
+	err = c.FindId(id).One(result)
+	if err != nil {
+		err = ParseError(err)
+		return
+	}
+
+	return
+}
+
 func (c *Collection) Commit(id interface{}, data interface{}) (err error) {
 	err = c.UpdateId(id, bson.M{
 		"$set": data,
