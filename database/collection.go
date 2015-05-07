bc284443de10f0cfea62df882e444122cff13aea
Zachary Huff <zach.huff.386@gmail.com>
2015-05-06 22:12:15 -0400

    Add commit fields to collection

--- database/collection.go
+++ database/collection.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"github.com/dropbox/godropbox/container/set"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
+	"reflect"
+	"strings"
 )
 
 type Collection struct {
@@ -11,7 +14,8 @@ type Collection struct {
 }
 
 func (c *Collection) FindOne(query interface{}, result interface{}) (
-err error) {
+	err error) {
+
 	err = c.Find(query).One(result)
 	if err != nil {
 		err = ParseError(err)
@@ -22,7 +26,8 @@ err error) {
 }
 
 func (c *Collection) FindOneId(id interface{}, result interface{}) (
-err error) {
+	err error) {
+
 	err = c.FindId(id).One(result)
 	if err != nil {
 		err = ParseError(err)
@@ -39,6 +44,50 @@ func (c *Collection) Commit(id interface{}, data interface{}) (err error) {
 	if err != nil {
 		err = ParseError(err)
 		return
+	}
+
+	return
+}
+
+func (c *Collection) CommitFields(id interface{}, data interface{},
+	fields set.Set) (err error) {
+
+	err = c.UpdateId(id, bson.M{
+		"$set": SelectFields(data, fields),
+	})
+	if err != nil {
+		err = ParseError(err)
+		return
+	}
+
+	return
+}
+
+func SelectFields(obj interface{}, fields set.Set) (data bson.M) {
+	val := reflect.ValueOf(obj).Elem()
+	data = bson.M{}
+
+	n := val.NumField()
+	for i := 0; i < n; i++ {
+		typ := val.Type().Field(i)
+
+		if typ.PkgPath != "" {
+			continue
+		}
+
+		tag := typ.Tag.Get("bson")
+		if tag == "" || tag == "-" {
+			continue
+		}
+		tag = strings.Split(tag, ",")[0]
+
+		if !fields.Contains(tag) {
+			continue
+		}
+
+		val := val.Field(i).Interface()
+
+		data[tag] = val
 	}
 
 	return
