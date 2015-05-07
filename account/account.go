c7bafcac9cc1bb5b8922b0aeca6538457f8c4a0d
Zachary Huff <zach.huff.386@gmail.com>
2015-05-07 03:17:09 -0400

    Fix exp type

--- account/account.go
+++ account/account.go
@@ -1,10 +1,14 @@
 // Oauth account.
 package account
 
+import (
+	"time"
+)
+
 type Account struct {
-	Id            string `bson:"_id"`
-	Type          string `bson:"type"`
-	Oauth2AccTokn string `json:"oauth2_acct_tokn"`
-	Oauth2RefTokn string `json:"oauth2_ref_tokn"`
-	Oauth2Exp     string `json:"oauth2_exp"`
+	Id            string    `bson:"_id"`
+	Type          string    `bson:"type"`
+	Oauth2AccTokn string    `json:"oauth2_acct_tokn"`
+	Oauth2RefTokn string    `json:"oauth2_ref_tokn"`
+	Oauth2Exp     time.Time `json:"oauth2_exp"`
 }
