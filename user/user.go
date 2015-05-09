eac25c54e84b83e46d47f13285f22a3465c6566b
Zachary Huff <zach.huff.386@gmail.com>
2015-05-09 03:25:15 -0400

    Add user

---
+++ user/user.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"github.com/pritunl/pritunl-auth/database"
+)
+
+type User struct {
+	Id          string `bson:"_id"`
+	Status      string `bson:"status"`
+	Plan        string `bson:"plan"`
+	LicenseHash string `bson:"license_hash"`
+}
+
+func FindUser(db *database.Database, id string) (usr *User, err error) {
+	usr = &User{}
+	coll := db.Users()
+
+	err = coll.FindOneId(id, usr)
+	if err != nil {
+		return
+	}
+
+	return
+}
