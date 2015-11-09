58ec0e6bd65a92681fa61cea20c947e9561aebd8
Zachary Huff <zach.huff.386@gmail.com>
2015-11-09 03:47:44 -0500

    Init sign cert in main

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"flag"
 	"github.com/pritunl/pritunl-auth/cmd"
 	"github.com/pritunl/pritunl-auth/requires"
+	"github.com/pritunl/pritunl-auth/saml"
 )
 
 func main() {
 	requires.Init()
 	flag.Parse()
+
+	err := saml.InitSignCert()
+	if err != nil {
+		panic(err)
+	}
 
 	switch flag.Arg(0) {
 	case "app":
